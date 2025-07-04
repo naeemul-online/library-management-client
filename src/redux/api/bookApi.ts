deb847e1aefbf6a7ac6f0156ecc34004eda29875
naeemul-online <naeemul.online@gmail.com>
2025-07-04 18:30:57 +0300

    uploaded to vercel

--- src/redux/api/bookApi.ts
+++ src/redux/api/bookApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const bookApi = createApi({
   reducerPath: "bookApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5001/api/",
+    baseUrl: "https://library-management-api-gold.vercel.app/api/",
   }),
   tagTypes: ["Book", "Borrow"],
   endpoints: (builder) => ({
